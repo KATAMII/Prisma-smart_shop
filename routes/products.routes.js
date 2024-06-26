12367272e6f8151689801a757652cb17e088efa3
KATAMII <lydiamwaah01@gmail.com>
2024-06-26 16:04:37 +0300

    add the get update the products

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -9,26 +9,31 @@ router.get("/",async(req,res) =>{
       const products = await prisma.products.findMany();
       res.status(201).json(products)
     } catch (e) {
-        res.status(500).json({success:false,message:e.message})
+        res.status(500).json({success:false,message:["error connecting to the server"]})
     }
 })
 router.get("/:id",async(req,res) =>{
     const id=req.params.id;
     try {
-       const products = await prisma.products.findFirst({
+       const product = await prisma.products.findFirst({
         where :{id:id},
         select:{
+            id:true,
             productThumbnail:true,
             productTitle:true,
             productDescription:true,
             productCost: true,
             onOffer:true
         }
-
        });
-       res.status(201).json(products)
+       if(!product){
+        res.status(404).json({success:false,message:["product not found"]})
+       }else{
+        res.status(201).json(product)
+       }
+       
     } catch (e) {
-        res.status(500).json({success:false,message:["products not found"]})
+        res.status(500).json({success:false,message:["product not found"]})
     }
 })
 router.post("/",async(req,res) =>{
@@ -49,11 +54,56 @@ router.post("/",async(req,res) =>{
         res.status(500).json({success:false, message: e.message})
     }
 })
-router.patch("/:id",(req,res) =>{
-    res.send("updating a product")
+router.patch("/:id",async(req,res) =>{
+    const {productThumbnail,productTitle,productDescription,productCost,onOffer}=req.body;
+    const id=req.params.id;
+    try {
+        let updateUser;
+        if(productTitle){
+            updateUser = await prisma.products.update({
+                where:{id:id},
+                data:{productTitle:productTitle} 
+            })
+        }
+        if(productThumbnail){
+            updateUser = await prisma.products.update({
+                where:{id:id},
+                data:{productThumbnail:productThumbnail} 
+            })
+        }
+        if(productDescription){
+            updateUser = await prisma.products.update({
+                where:{id:id},
+                data:{productDescription:productDescription} 
+            })
+        }
+        if(productCost){
+            updateUser = await prisma.products.update({
+                where:{id:id},
+                data:{productCost:productCost} 
+            })
+        }
+        if(onOffer){
+            updateUser = await prisma.products.update({
+                where:{id:id},
+                data:{onOffer:onOffer} 
+            })
+        }
+        res.status(200).json(updateUser);
+    } catch (e) {
+        res.status(500).json({success:false,message:["updating not successful"]})
+    }
 })
-router.delete("/:id",(req,res) =>{
-    res.send("deleting  a product")
+router.delete("/:id",async(req,res) =>{
+    const id = req.params.id;
+    try {
+        const deleteProduct = await prisma.products.delete({
+            where:{id:id}
+        })
+        res.status(200).json(deleteProduct)
+    } catch (e) {
+       res.status(500).json({success:false ,message:["user doesnt exist"]})
+    }
 })
 
 export default router
