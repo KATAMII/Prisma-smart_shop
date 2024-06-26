fa5077512b71187cdb1944596775bb7c672a27c2
KATAMII <lydiamwaah01@gmail.com>
2024-06-26 13:03:21 +0300

    add the create product functionality

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express' 
+import{config} from 'dotenv'
+config();
 import productsRouter from './routes/products.routes.js'
 
 const app = express();
-app.use("/products",productsRouter)
+app.use(express.json());
+app.use("/products",productsRouter);
 app.listen(3001,()=>{
     console.log('server is running on port 3001')
 })
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -10,8 +10,22 @@ router.get("/",(req,res) =>{
 router.get("/:id",(req,res) =>{
     res.send("getting a single the products")
 })
-router.post("/",(req,res) =>{
-    res.send("creating a product")
+router.post("/",async(req,res) =>{
+    try {
+        const {productThumbnail,productTitle,productDescription,productCost,onOffer}=req.body;
+        const newproduct = await prisma.products.create({
+            data:{
+                productThumbnail:productThumbnail,
+                productTitle:productTitle,
+                productDescription:productDescription,
+                productCost: productCost,
+                onOffer:onOffer
+            }
+        })
+        res.status(201).json(newproduct);
+    } catch (e) {
+        res.status(500).json({success:false, message: e.message})
+    }
 })
 router.patch("/:id",(req,res) =>{
     res.send("updating a product")
